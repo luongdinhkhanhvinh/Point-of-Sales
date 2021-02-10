e39a9882ca10762096bd37e922430d27a8a03e4d
luongdinhkhanhvinh <luongdkvinh@gmail.com>
2021-02-11 00:19:34 +0700

    create fragment login page

---
+++ PosApp/PosApp/frmLogin.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PosApp
+{
+    /// <summary>
+    /// Interaction logic for frmLogin.xaml
+    /// </summary>
+    public partial class frmLogin : Window
+    {
+
+        public static string loggedIn;
+        public static string _name;
+        public static string _surname;
+        public static string _password;
+        public static string _gender;
+        public static string _dob;
+        public static int _id;
+        public static string _img;
+        public static string _usertype;
+        public bool _isActive = false;
+
+        public bool result;
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
